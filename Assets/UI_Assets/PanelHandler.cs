bb1c918213ab5e452165c5d2ff56e8c9fe7bf728
char1ro <93292604+char1ro@users.noreply.github.com>
2023-02-19 10:59:55 -0500

    Connection

--- Assets/UI_Assets/PanelHandler.cs
+++ Assets/UI_Assets/PanelHandler.cs
@@ -8,7 +8,6 @@ public class PanelHandler : MonoBehaviour
     public GameObject p1;
     public GameObject p2;
     public GameObject p3;
-    public GameObject p4;
     public Button SartB;
     public Button InstructionB;
     public Button SettingB;
@@ -29,48 +28,42 @@ public class PanelHandler : MonoBehaviour
         switch (p)
         {
             case 0:
-                p1.SetActive(true);
-                p2.SetActive(false);
+                p1.SetActive(false);
                 SartB.interactable = true;
                 InstructionB.interactable = true;
                 SettingB.interactable = true;
                 CreditB.interactable = true;
                 break;
             case 1:
-                p1.SetActive(false);
-                p2.SetActive(true);
+                p1.SetActive(true);
                 SartB.interactable = false;
                 InstructionB.interactable = false;
                 SettingB.interactable = false;
                 CreditB.interactable = false;
                 break;
             case 2:
-                p1.SetActive(true);
-                p3.SetActive(false);
+                p2.SetActive(false);
                 SartB.interactable = true;
                 InstructionB.interactable = true;
                 SettingB.interactable = true;
                 CreditB.interactable = true;
                 break;
             case 3:
-                p1.SetActive(false);
-                p3.SetActive(true);
+                p2.SetActive(true);
                 SartB.interactable = false;
                 InstructionB.interactable = false;
                 SettingB.interactable = false;
                 CreditB.interactable = false;
                 break;
             case 4:
-                p1.SetActive(true);
-                p4.SetActive(false);
+                p3.SetActive(false);
                 SartB.interactable = true;
                 InstructionB.interactable = true;
                 SettingB.interactable = true;
                 CreditB.interactable = true;
                 break;
             case 5:
-                p1.SetActive(false);
-                p4.SetActive(true);
+                p3.SetActive(true);
                 SartB.interactable = false;
                 InstructionB.interactable = false;
                 SettingB.interactable = false;
